993e075b0ba0c65d4159112ca184b38c54d2f3f0
chenyongping <chenyongping001@gmail.com>
2023-06-17 21:34:44 +0800

    Reflactor: Remove duplicated sytles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="260px" borderRadius={10} overflow="hidden" height="280px">
-      <Skeleton />
+    <Card>
+      <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
       </CardBody>

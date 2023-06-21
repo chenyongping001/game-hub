e2e0fea74474fa37f0aba5f2cd7ba37e775f4d6e
chenyongping <chenyongping001@gmail.com>
2023-06-21 21:58:47 +0800

    Add Emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import meh from "../assets/meh.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import bullsEye from "../assets/bulls-eye.webp";
+
+interface Props {
+  rating_top: number;
+}
+
+const Emoji = ({ rating_top }: Props) => {
+  if (rating_top < 3) return;
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
+  return <Image {...emojiMap[rating_top]} marginTop={1} />;
+};
+
+export default Emoji;

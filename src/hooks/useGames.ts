286056f4021031bf9fa416e8b570a71a6d531cec
chenyongping <chenyongping001@gmail.com>
2023-06-21 11:49:47 +0800

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,6 +18,7 @@ const useGames = (gameQuery:GameQuery)=>useData<Game>('/games',{params:{
     genres:gameQuery.genre?.id,
     platforms:gameQuery.platform?.id,
     ordering:gameQuery.sortOrder,
+    search:gameQuery.searchText
 }},[gameQuery]);
 
 export default useGames;

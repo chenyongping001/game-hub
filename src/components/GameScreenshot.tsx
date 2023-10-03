abab4adcc44f2653e8c272604d7095591a5074db
chenyongping <chenyongping001@gmail.com>
2023-10-03 12:14:31 +0800

    Build game screenshot

---
+++ src/components/GameScreenshot.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import useScreenshots from "../hooks/useScreenshots";
+import { Image, SimpleGrid } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+const GameScreenshot = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useScreenshots(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((s) => (
+        <Image key={s.id} src={s.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+export default GameScreenshot;
